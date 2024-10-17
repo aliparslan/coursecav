6140fa0bc1ea338b221d851cb04f9308d1cdc1eb
Alip Arslan <aliparslan@outlook.com>
2024-10-16 22:30:03 -0400

    add db connection

---
+++ app/notes/page.tsx
@@ -0,0 +1,14 @@
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Notes() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from("notes").select();
+  const { data: test_users } = await supabase.from("test_users").select();
+
+  return (
+    <div>
+      {/* <pre>{JSON.stringify(notes, null, 2)}</pre> */}
+      <pre>{JSON.stringify(test_users, null, 2)}</pre>
+    </div>
+  )
+}
