fe31b63e7b103cb860750229ff35aec3e3e5a8e6
Alip Arslan <aliparslan@outlook.com>
2024-10-16 22:41:59 -0400

    fix vercel build

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Notes() {
   const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  // const { data: notes } = await supabase.from("notes").select();
   const { data: test_users } = await supabase.from("test_users").select();
 
   return (
